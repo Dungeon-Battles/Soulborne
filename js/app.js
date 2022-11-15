825bcb67727a650ba62594e7b8ae715de54581b7
Firebite2 <100682828+Firebite2@users.noreply.github.com>
2022-11-15 18:16:15 +0100

    new mouse

--- js/app.js
+++ js/app.js
@@ -1,36 +1,14 @@
-const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav-links');
-	
-	burger.addEventListener('click', () => {
-		nav.classList.toggle('nav-active');
-	});
-}
-
-navSlide();
-
 window.addEventListener('load', () => {
 	const preload = document.querySelector('.preload');
 	preload.classList.add("preload-finish");
 });
 
-function mOver1(obj){
-	obj.src = "./img/sl-home.png";
-}
-function mOut1(obj){
-	obj.src = "./img/home.png";
-}
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll('.nav-links li');
 
-function mOver2(obj){
-	obj.src = "./img/sl-update.png";
-}
-function mOut2(obj){
-	obj.src = "./img/Update.png";
-}
+window.addEventListener("mousemove", cursor);
 
-function mOver3(obj){
-	obj.src = "./img/sl-about-us.png";
-}
-function mOut3(obj){
-	obj.src = "./img/about-us.png";
+function cursor(e){
+	mouseCursor.style.top = e.pageY + 'px';
+	mouseCursor.style.left = e.pageX + 'px';
 }
---
+++ js/navbar.js
@@ -0,0 +1,34 @@
+
+//hamburger menu
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active');
+	});
+}
+
+navSlide();
+
+//reactive buttons
+function mOver1(obj){
+	obj.src = "./img/sl-home.png";
+}
+function mOut1(obj){
+	obj.src = "./img/home.png";
+}
+
+function mOver2(obj){
+	obj.src = "./img/sl-update.png";
+}
+function mOut2(obj){
+	obj.src = "./img/update.png";
+}
+
+function mOver3(obj){
+	obj.src = "./img/sl-about-us.png";
+}
+function mOut3(obj){
+	obj.src = "./img/about-us.png";
+}
